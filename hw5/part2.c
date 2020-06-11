cfb087cc4fb0682126b18efbefc73b9bc7703ed8
daniellamb2208 <lanmb@.localdomain>
2020-06-11 20:41:53 +0800

    add comment

--- hw5/part2.c
+++ hw5/part2.c
@@ -9,7 +9,7 @@
 #include <time.h>
 
 #define true 1
-#define length 1024
+#define length 1024 //fixed(max) file size
 #define handel_error(msg)   \
     do                      \
     {                       \
@@ -21,7 +21,7 @@ void create(int mode)
 {
     char *addr;
     int fd;
-    struct stat sb;
+    struct stat sb; //store state of file (descriptor fd)
 
     fd = open("mmap.txt", O_RDWR | O_CREAT);
     if (fd == -1)
@@ -29,21 +29,21 @@ void create(int mode)
     if (fstat(fd, &sb) == -1)
         handel_error("fstat error\n");
 
-    lseek(fd, length + 1, SEEK_SET);
+    lseek(fd, length + 1, SEEK_SET); //extent file
     write(fd, "", 1);
-    lseek(fd, 0, SEEK_SET);
+    lseek(fd, 0, SEEK_SET); //points to the begining of file
 
     addr = mmap(0, length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED)
         handel_error("mmap failure\n");
 
-    if (mode == 1)
+    if (mode == 1) //random write
     {
         char r = (rand() % 26 + 65);
-        sprintf(addr, "%c\n", r);
+        sprintf(addr, "%c\n", r); //write(change) memory
         printf("txt is now %c\n", r);
     }
-    else
+    else //customal write (and it's default, if wrong input)
     {
         char txt[1024];
         printf("please enter your txt: ");
@@ -52,7 +52,7 @@ void create(int mode)
         printf("txt is %s now\n", txt);
     }
 
-    msync(addr, length, MS_SYNC);
+    msync(addr, length, MS_SYNC); //make sure synchronous into disk
     munmap(addr, length);
     close(fd);
     printf("\n");
